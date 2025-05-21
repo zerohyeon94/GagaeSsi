8c8a054c6b754597f025a9bdbe0f7bdc27ccf3ce
JoYeongHyeon <yhjo@ismartsound.com>
2025-05-21 17:09:59 +0900

    feat: initialize GagaeSsi project with UIKit

---
+++ GagaeSsi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GagaeSsi
+//
+//  Created by 조영현 on 5/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
