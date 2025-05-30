be98bea451ffa889243b1c027a66b972ccc9c42f
JoYeongHyeon <yhjo@ismartsound.com>
2025-05-30 11:24:20 +0900

    chore: add lifecycle logging to BaseViewController

--- GagaeSsi/Presentation/BaseViewController.swift
+++ GagaeSsi/Presentation/BaseViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        logLifeCycle("viewDidLoad")
         setupTapToDismissKeyboard()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        logLifeCycle("viewWillAppear")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logLifeCycle("viewDidAppear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        logLifeCycle("viewWillDisappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        logLifeCycle("viewDidDisappear")
+    }
+
+    private func logLifeCycle(_ methodName: String) {
+        let className = String(describing: type(of: self))
+        print("🌀 \(className) - \(methodName)")
+    }
+
+    // MARK: - Keyboard Dismiss
     private func setupTapToDismissKeyboard() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
